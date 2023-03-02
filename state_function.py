b440bf7525896d919a9485603b0f4fda4b98cfcb
Fokoue22 <fokouethomasdylan@gmail.com>
2023-03-02 23:22:20 +0100

    test11

--- state_function.py
+++ state_function.py
@@ -6,6 +6,8 @@ import os
 import csv
 import schedule
 import time
+from pathlib import Path 
+from dotenv import load_dotenv 
 from botocore.exceptions import ClientError
 from email import encoders
 from email.mime.base import MIMEBase
@@ -19,7 +21,7 @@ logger = logging.getLogger()
 
 # global variable 
 FILE_NAME = '/tmp/' + 'ec2-report.csv'
-SLACK_TOKEN = 'xoxb-4796957867426-4878973368866-Jb6J1YXtqtfHMP3JMQrn3j1u'
+# SLACK_TOKEN = 'xoxb-4796957867426-4878973368866-Jb6J1YXtqtfHMP3JMQrn3j1u'
 SNAPSHOT_NAME = 'snapshot_list'
 TERMINATE = 'statefilters'
 
@@ -189,9 +191,9 @@ def send_slack_message():
 
    ## creating ebs Snapshot that only create snapshot for stopped ec2 instance
     EMAIL = 'willcabrel735@gmail.com'
-    # env_path = Path('.') / '.env'
-    # load_dotenv(dotenv_path = env_path)
-    client = slack.WebClient(token= SLACK_TOKEN)
+    env_path = Path('.') / '.env'
+    load_dotenv(dotenv_path = env_path)
+    client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
     client.chat_postMessage(channel="#general", text= (f'Hello sir,\n\n And Ec2 report was generated, it containe {FILE_NAME}\n\n{SNAPSHOT_NAME} and was send to this email:\n{EMAIL}\n\nThanks, \n\nFokoue Thomas!'))
     
 # schedule.every(10).seconds.do(send_slack_message)
